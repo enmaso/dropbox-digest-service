d73933ebb1587b97412063a5db3b2e7013194047
Adam Jones <adam@enmaso.com>
2017-11-01 16:45:45 -0700

    swap request for dropbox api

--- dropbox-digest.js
+++ dropbox-digest.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 const mongoose = require('mongoose')
-const request = require('request')
+const Dropbox = require('dropbox')
 const AWS = require('aws-sdk')
 const Consumer = require('sqs-consumer')
 const async = require('async')
@@ -66,130 +66,123 @@ function processFile (message, done) {
         return done('service null')
       } else {
         // Download the file
-        let opts = {
-          url: file.downloadUrl,
-          headers: {
-            'Authorization': 'Bearer ' + service.token,
-            'Dropbox-API-Arg': `{'path': '${file.source.path_lower}'}`
-          },
-          encoding: null
-        }
-        request(opts, function (err, response, data) {
-          if (err) {
-            console.error(err)
-            return done(err)
-          } else if (response.statusCode !== 200) {
-            console.error(`Google Drive response ${response.statusCode} ${response.statusMessage}`)
-            return done(response.statusCode)
-          } else {
-            // PROCESS THE FILE
-            let tmp = `/tmp/${file._id.toString()}${file.filename.substring(file.filename.lastIndexOf('.'))}`
-            fs.writeFile(tmp, data, 'binary', function (err) {
-              if (err) {
-                console.error(err)
-                return done(err)
-              } else {
-                async.series([
-                  function (next) {
-                    // Extract file metadata with Tika
-                    exec(`curl -T ${tmp} ${TIKA_META} --header 'Accept: application/json'`, (err, stdout, stderr) => {
-                      if (err) {
-                        console.error(err)
-                      } else {
-                        file.metadata = JSON.parse(stdout)
+        let dropbox = new Dropbox({
+          accessToken: service.token
+        })
+        dropbox.filesDownload({
+          path: file.source.path_lower
+        })
+        .then(data => {
+          let tmp = `/tmp/${file._id.toString()}${file.filename.substring(file.filename.lastIndexOf('.'))}`
+          fs.writeFile(tmp, data.fileBinary, 'binary', function (err) {
+            if (err) {
+              console.error(err)
+              return done(err)
+            } else {
+              async.series([
+                function (next) {
+                  // Extract file metadata with Tika
+                  exec(`curl -T ${tmp} ${TIKA_META} --header 'Accept: application/json'`, (err, stdout, stderr) => {
+                    if (err) {
+                      console.error(err)
+                    } else {
+                      file.metadata = JSON.parse(stdout)
 
-                        file.title = file.metadata.title || ''
-                        file.creationDate = file.metadata['Creation-Date'] || ''
-                        file.lastSaveDate = file.metadata['Last-Save-Date'] || ''
-                        file.lastModified = file.metadata['Last-Modified'] || ''
-                        file.creator = file.metadata['dc:creator'] || ''
-                        file.lastAuthor = file.metadata['Last-Author'] || ''
-                        file.publisher = file.metadata.publisher || ''
+                      file.title = file.metadata.title || ''
+                      file.creationDate = file.metadata['Creation-Date'] || ''
+                      file.lastSaveDate = file.metadata['Last-Save-Date'] || ''
+                      file.lastModified = file.metadata['Last-Modified'] || ''
+                      file.creator = file.metadata['dc:creator'] || ''
+                      file.lastAuthor = file.metadata['Last-Author'] || ''
+                      file.publisher = file.metadata.publisher || ''
 
-                        file.save(err => {
-                          if (err) {
-                            console.log(err)
-                          }
-                        })
-                      }
-                      next()
-                    })
-                  },
-                  function (next) {
-                    // Extract file text with Tika
-                    exec(`curl -T ${tmp} ${TIKA_TEXT}`, (err, stdout, stderr) => {
-                      if (err) {
-                        console.error(err)
-                      } else {
-                        file.text = stdout.replace(/\r?\n|\r|\t/g, ' ').trim()
-                        file.save(err => {
-                          if (err) {
-                            console.error(err)
-                          }
-                        })
-                      }
-                      next()
-                    })
-                  },
-                  function (next) {
-                    // Extract NER from file text
-                    if (file.text.length === 0) {
-                      console.log(`No text to be extracted from ${tmp}`)
-                      return next()
-                    }
-                    let socket = new net.Socket()
-                    socket.connect(process.env.NER_PORT, 'localhost', () => {
-                      socket.write(file.text + '\n')
-                    })
-                    socket.on('data', data => {
-                      let regexp = /<([A-Z]+?)>(.+?)<\/\1>/g
-                      let str = data.toString()
-                      let tags = {
-                        LOCATION: [],
-                        ORGANIZATION: [],
-                        DATE: [],
-                        MONEY: [],
-                        PERSON: [],
-                        PERCENT: [],
-                        TIME: []
-                      }
-                      let m
-                      while ((m = regexp.exec(str)) !== null) {
-                        if (m.index === regexp.lastIndex) {
-                          regexp.lastIndex++
+                      file.save(err => {
+                        if (err) {
+                          console.log(err)
                         }
-                        tags[m[1]].push(m[2])
-                      }
-                      socket.destroy()
-                      file.ner = tags
+                      })
+                    }
+                    next()
+                  })
+                },
+                function (next) {
+                  // Extract file text with Tika
+                  exec(`curl -T ${tmp} ${TIKA_TEXT}`, (err, stdout, stderr) => {
+                    if (err) {
+                      console.error(err)
+                    } else {
+                      file.text = stdout.replace(/\r?\n|\r|\t/g, ' ').trim()
                       file.save(err => {
                         if (err) {
                           console.error(err)
                         }
                       })
-                      next()
+                    }
+                    next()
+                  })
+                },
+                function (next) {
+                  // Extract NER from file text
+                  if (file.text.length === 0) {
+                    console.log(`No text to be extracted from ${tmp}`)
+                    return next()
+                  }
+                  let socket = new net.Socket()
+                  socket.connect(process.env.NER_PORT, 'localhost', () => {
+                    socket.write(file.text + '\n')
+                  })
+                  socket.on('data', data => {
+                    let regexp = /<([A-Z]+?)>(.+?)<\/\1>/g
+                    let str = data.toString()
+                    let tags = {
+                      LOCATION: [],
+                      ORGANIZATION: [],
+                      DATE: [],
+                      MONEY: [],
+                      PERSON: [],
+                      PERCENT: [],
+                      TIME: []
+                    }
+                    let m
+                    while ((m = regexp.exec(str)) !== null) {
+                      if (m.index === regexp.lastIndex) {
+                        regexp.lastIndex++
+                      }
+                      tags[m[1]].push(m[2])
+                    }
+                    socket.destroy()
+                    file.ner = tags
+                    file.save(err => {
+                      if (err) {
+                        console.error(err)
+                      }
                     })
-                  },
-                  function (next) {
-                    // Extract keywords from text
-                    // Using gramophone for now, will want to use NaturalNode or other later...
-                    file.keywords = gramophone.extract(file.text, { limit: 15 })
                     next()
-                  },
-                  function (next) {
-                    // Convert file to image, if possible
-                    next()
-                  }
-                ], (err, result) => {
-                  if (err) {
-                    console.err(err)
-                  }
-                  done()
-                  fs.unlinkSync(tmp)
-                })
-              }
-            })
-          }
+                  })
+                },
+                function (next) {
+                  // Extract keywords from text
+                  // Using gramophone for now, will want to use NaturalNode or other later...
+                  file.keywords = gramophone.extract(file.text, { limit: 15 })
+                  next()
+                },
+                function (next) {
+                  // Convert file to image, if possible
+                  next()
+                }
+              ], (err, result) => {
+                if (err) {
+                  console.err(err)
+                }
+                done()
+                fs.unlinkSync(tmp)
+              })
+            }
+          })
+        })
+        .catch(err => {
+          console.error(err)
+          done(err)
         })
       }
     })
@@ -203,23 +196,3 @@ function processFile (message, done) {
     done(err)
   })
 }
-
-/*
-      // Refresh access tokens if needed
-      //sdk.getTokensRefreshGrant(service.refreshToken, (err, tokens) => {
-      sdk.getTokensRefreshGrant(service.refresh, (err, tokens) => {
-        if (err) {
-          console.error(err)
-          return done(err)
-        } else {
-          service.token = tokens.accessToken
-          service.refresh = tokens.refreshToken
-          service.save(err => {
-            if (err) {
-              console.error(err)
-            }
-          })
-
-        }
-      })
-*/
